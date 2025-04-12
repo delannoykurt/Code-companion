1249600d006ba4738497048ad8ccbeac16299d06
delannoykurt <delannoykurt666@gmail.com>
2025-04-12 11:17:01 +0200

    modify implementation and change lib

--- bot.py
+++ bot.py
@@ -1,14 +1,14 @@
-import discord
-import random
-
-from dotenv import load_dotenv
 import os
+import random
+import nextcord
+from nextcord.ext import commands
+from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-intents = discord.Intents.default()
-bot = discord.Bot(intents=intents)
+intents = nextcord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 ressources = [
     "https://overapi.com/javascript",
@@ -21,11 +21,9 @@ ressources = [
 async def on_ready():
     print(f"✅ Connecté en tant que {bot.user}")
 
-@bot.command(description="Obtiens une ressource aléatoire")
+@bot.command(name="ressource", help="Affiche une ressource aléatoire")
 async def ressource(ctx):
     lien = random.choice(ressources)
-    await ctx.respond(f"📘 Voici une ressource : {lien}")
-
+    await ctx.send(f"📘 Voici une ressource : {lien}")
 
 bot.run(TOKEN)
-
